e492281fca1382621455164262be1da0e1d6ee61
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2022-09-22 13:05:54 +0300

    result

---
+++ src/refs/index.js
@@ -0,0 +1,2 @@
+export const listCardRef = document.querySelector(".js-list-card");
+export const listBtnRef = document.querySelector(".js-list-btn");
--- src/theory.js
+++ src/theory.js
@@ -12,3 +12,26 @@
 // GET	Операція read - отримати набір ресурсів або один ресурс за ідентифікатором.
 // PUT и PATCH	Операція update - оновити ресурс за ідентифікатором.
 // DELETE	Операція delete - видалити ресурс за ідентифікатором
+// import axios from "axios";
+
+// axios.defaults.baseURL = "http://localhost:3000";
+// //GET
+// axios.get("/data").then(console.log);
+
+//POST
+// axios.post("/data", { value: "Купити помідорів!" });
+
+// axios({
+//   method: "post",
+//   url: "/data",
+//   data: {
+//     firstName: "Fred",
+//     lastName: "Flintstone",
+//   },
+// });
+
+//PUT
+// axios.put("http://localhost:3000/data/2", { value: "Купити огірків" });
+
+//PATCH
+// axios.patch("http://localhost:3000/data/3", { value: "Купити риби!" });
